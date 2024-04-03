dabcc5d90eb63db89a5682b5ae54b04f50847c29
flslin <84002788+flslin@users.noreply.github.com>
2024-04-03 17:52:25 +0900

    아이템 움직임

--- Assets/Scripts/ItemBomb.cs
+++ Assets/Scripts/ItemBomb.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class ItemBomb : MonoBehaviour
 {
     public GameObject bomb;
-    public float moveX;
+    public float moveX = 1;
     public float moveY;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +18,17 @@ public class ItemBomb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector2(moveX, moveY));
-    }
+        if (moveX < 2.4)
+            moveX += Time.deltaTime * speed;
+        else if (moveX > -2.4)
+            moveX -= Time.deltaTime * speed;
 
+        if (moveY < 4.4)
+            moveY += Time.deltaTime * speed;
+        else if (moveY > -4.4)
+            moveY -= Time.deltaTime * speed;
+
+        bomb.transform.Translate(moveX * speed * Time.deltaTime, moveY * speed * Time.deltaTime, 1);
+    }
 
 }
